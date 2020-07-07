a94a854462b7c25cc0160920ab434c776281418f
Shakif95 <shakif.malek@gmail.com>
2020-07-07 17:11:51 +0600

    pricing page ready

--- src/gatsby-theme-flex/schemes.js
+++ src/gatsby-theme-flex/schemes.js
@@ -13,33 +13,6 @@ import Venture from '../images/packages/venture.svg';
 
 import { useState } from 'react';
 
-const BaseStyles = {
-  btn: {
-    paddingX: '40px',
-    paddingY: '10px'
-  },
-}
-
-const styles = {
-  btnLeftSharp: {
-    paddingX: BaseStyles.btn.paddingX,
-    paddingY: BaseStyles.btn.paddingY,
-    borderRadius: '3px 0 0 3px'
-  },
-  btnSharp: {
-    paddingX: BaseStyles.btn.paddingX,
-    paddingY: BaseStyles.btn.paddingY,
-    borderRadius: '0'
-  },
-  btnRightSharp: {
-    paddingX: BaseStyles.btn.paddingX,
-    paddingY: BaseStyles.btn.paddingY,
-    borderRadius: '0 3px 3px 0'
-  },
-  cardIcon: {
-    marginY: '25px'
-  }
-}
 
 const marks = [
   {
@@ -216,49 +189,12 @@ export const Schemes = () => {
       </div>
       <div sx={{ maxWidth: 'container', margin: 'auto' }}>
         <Grid columns={[1, null, 3]} gap={10}>
-          {selectedPriceCards && selectedPriceCards.map(card => <div key={card.key}>{card.value}</div>)}
-          {/* <PriceCard
-              icon={<GiDirectorChair size={50} sx={styles.cardIcon} />}
-              header='Starter'
-              price='€129/m'
-              footerText='Monthly expenses below €3k'
-            />
+          {
+            selectedPriceCards && selectedPriceCards.map(card =>
+              <div key={card.key}>{card.value}</div>
+              )
+          }
 
-            <PriceCard
-              icon={<FaWarehouse size={50} sx={styles.cardIcon} />}
-              header='Micro'
-              price='$159/m'
-              footerText='Monthly expenses more than €3k to €10k'
-            />
-
-            <PriceCard
-              icon={<AiOutlineHome size={50} sx={styles.cardIcon} />}
-              header='Boutique'
-              price='$199/m'
-              footerText='Monthly expenses more than €10k to €20k'
-              disabled={true}
-            />
-
-            <PriceCard
-              icon={<RiBuilding3Line size={50} sx={styles.cardIcon} />}
-              header='Venture'
-              price='1% of expenses/m'
-              footerText='Monthly expenses more than €20k to €100k'
-            />
-
-            <PriceCard
-              icon={<GiFamilyHouse size={50} sx={styles.cardIcon} />}
-              header='Corporate'
-              price='0,8% of expenses/m'
-              footerText='Monthly expenses more than €100k to €150k'
-            />
-
-            <PriceCard
-              icon={<FaRegBuilding size={50} sx={styles.cardIcon} />}
-              header='Elite'
-              price='0,6% of expenses/m'
-              footerText='Monthly expenses more than €150k'
-            /> */}
         </Grid>
       </div>
     </section>
