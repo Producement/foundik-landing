37fee74c647f54f165e6656a8d422dd3480d40a1
Shakif95 <shakif.malek@gmail.com>
2020-08-25 15:37:37 +0600

    maksekeskus link added to terms

--- src/gatsby-theme-flex/terms.js
+++ src/gatsby-theme-flex/terms.js
@@ -83,6 +83,8 @@ export const Terms = () => {
           <li>Mastercard</li>	
         </ul>
 
+        <p>This can be paid securely through <a href="http://www.maksekeskus.ee/">Maksekeskus AS</a> payment gateway.</p>
+
         You may be required to purchase or pay a fee to access some of our services. You agree to provide current, complete, and accurate purchase and account information for all purchases made via the Site. You further agree to promptly update account and payment information, including email address, payment method, and payment card expiration date, so that we can complete your transactions and contact you as needed. We bill you through an online billing account for purchases made via the Site. Sales tax will be added to the price of purchases as deemed required by us. We may change prices at any time. All payments shall be in Euros.
 
         You agree to pay all charges or fees at the prices then in effect for your purchases, and you authorize us to charge your chosen payment provider for any such amounts upon making your purchase. If your purchase is subject to recurring charges, then you consent to our charging your payment method on a recurring basis without requiring your prior approval for each recurring charge, until you notify us of your cancellation.
